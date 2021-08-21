0fff37e87ea8866134f0d094cba3b6b29c77ebdc
luhao <linesahao@outlook.com>
2021-08-21 17:59:33 +0800

    add random service

--- src/service/random.js
+++ src/service/random.js
@@ -1,15 +1,61 @@
+import Taro from "@tarojs/taro";
+
+var datas = [];
+var index = -1;
+
+const defaultLen = 20;
+
 const random = (pre) => {
-  return {
-    id: 1,
-    ch: {
-      path: "/zh/1-zh.mp3",
-      text: "所以土豆你是哪里人？"
-    },
-    en: {
-      path: "/zh/1-en.m4a",
-      text: "So Todd, where are you from?"
+  return new Promise((resolve, reject) => {
+    if(index == -1){
+      initRandomDatas(pre).then(dataList => {
+        datas = dataList;
+        resolve(nextData());
+      })
+    } else {
+      resolve(nextData());
     }
+  })
+}
+
+function initRandomDatas(pre){
+  const db = Taro.cloud.database();
+  return new Promise((resolve, reject) => {
+    db.collection('audios')
+      .count().then(res => {
+        var randomIds = [];
+        for(var i= 0; i< defaultLen; i++) {
+            randomIds[i] = randomId(pre, res.total);
+        }
+        const _ = db.command;
+        db.collection("audios").where({
+          id: _.in(randomIds)
+        }).get().then(res => {
+          let dataList = [];
+          res.data.forEach(function (item, index, array) {
+            dataList.push({id: item.id, ch: {path: item.chinese.audio_path, text: item.chinese.text}, en: {path: item.english.audio_path, text: item.english.text}});
+          });
+          resolve(dataList);
+        });
+      });
+  })
+}
+
+function nextData(){
+  index++;
+  if(index > datas.length){
+    initRandomDatas();
+    index = 0;
   }
+  return datas[index];
+}
+
+function randomId(pre, total){
+  var randomInt = Math.floor(Math.random() * total);
+  if(randomInt == pre){
+    randomInt = Math.floor(Math.random() * total);
+  }
+  return randomInt;
 }
 
 export {
