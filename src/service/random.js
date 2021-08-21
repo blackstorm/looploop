950da6dc47484d46716c1f5296d3613ba112bde4
浩 <inf2inf2@outlook.com>
2021-08-21 15:46:46 +0800

    add example

---
+++ src/service/random.js
@@ -0,0 +1,17 @@
+const random = (pre) => {
+  return {
+    id: 1,
+    ch: {
+      path: "/zh/1-zh.mp3",
+      text: "所以土豆你是哪里人？"
+    },
+    en: {
+      path: "/zh/1-en.m4a",
+      text: "So Todd, where are you from?"
+    }
+  }
+}
+
+export {
+  random
+}
