a817ac847bc4df127d09c79b2b0c4f1614bbad0f
luhao <linesahao@outlook.com>
2021-08-28 17:54:12 +0800

    fix audio count logic

--- app/src/service/userPlayRecords.js
+++ app/src/service/userPlayRecords.js
@@ -15,37 +15,15 @@ const record = ({ id, audio, lang }) => {
 const statistics = () => {
   const db = wx.cloud.database();
   return new Promise((resolve, reject) => {
-    db.collection("user_play_records")
-      .get()
-      .then((res) => {
-        console.log("get user play records", res);
-        // 查询记录
-        const _ = db.command;
-        const audioIds = res.data.map((it) => it.audio_id);
-        db.collection("audios")
-          .where({
-            id: _.in(audioIds),
-          })
-          .get()
-          .then((res2) => {
-            // 统计时长
-            const result = res2.data.reduce((r, it) => {
-              r[it.id] = it;
-              return r;
-            }, {});
-            let totalDuration = 0;
-            res.data.forEach((element) => {
-              const langaudio = isChinese(element.lang)
-                ? result[element.audio_id].chinese
-                : result[element.audio_id].english;
-              totalDuration += langaudio.duration;
-            });
-            resolve({
-              counts: audioIds ? audioIds.length : 0,
-              duration: totalDuration,
-            });
-          });
-      });
+    const $ = db.command.aggregate
+    db.collection("user_play_records").aggregate()
+    .group({
+      _id: null, 
+      counts: $.sum(1),
+      duration: $.sum("$duration")
+    }).end().then(res => {
+      resolve(res.list[0])
+    })
   });
 };
 
